4b140c9134e4cc4c99afa1f33194defd2b0fa35f
dcwiggins912 <dcwiggins912@gmail.com>
2015-07-29 04:18:30 -0400

    Removing files to fixi repository structure

--- www/wine_site/includes/layouts/header_logged_out.php
+++
@@ -1,7 +0,0 @@
-<div id="nav-bar">
-  <ul class="menu-hor">
-    <li><a href="index.php">Home</a></li>
-    <li>Item 2</li>
-    <li>Item 3</li>
-  </ul>
-</div>
--- www/wine_site/includes/layouts/logout_header.php
+++
@@ -1,6 +0,0 @@
-<div class="log-header">
-  <form action="index.php" method="post">
-    <label>Currently logged in as <b><?php echo $_SESSION["username"]; ?></b></label>
-    <input type="submit" name="logout" value="Log Out" style="width: 70px; "/>
-  </form>
-</div>
