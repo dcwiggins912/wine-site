7285ed7f223e44f4ba9884411687beb21932e0ef
dcwiggins912 <dcwiggins912@gmail.com>
2015-08-24 00:44:15 -0400

    Implemented core of search functionality

--- www/wine_site/public/wine_search.php
+++ www/wine_site/public/wine_search.php
@@ -6,16 +6,29 @@
 
 <?php
   if (isset($_GET)) {
-  
-    
+    $query  = "SELECT b.filename, a.name, a.varietal, a.vintage, a.region, a.country, a.description ";
+    $query .= "FROM wines a, wine_images b WHERE ";
+    $query .= "a.id = b.id ";
+    foreach ($_GET as $key => $value) {
+      $safe_value = mysql_prep($value);
+      $query .= "AND a.{$key} LIKE _utf8 '%{$safe_value}%' COLLATE utf8_unicode_ci ";
+    }
+    $query .= "LIMIT 10";
+    $result = mysqli_query($connection, $query);
+      
+    if ($result) {
+      $content = "";
+
+      while ($row = mysqli_fetch_assoc($result)) {
+        $content .= wine_as_div($row);
+      }
+    }
   }
   else {
     
   }
 
 ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
@@ -29,8 +42,9 @@
     
     <?php require_once("../includes/layouts/header.php"); ?>
     <h2>Search Wines</h2>
-    <br />
     <?php
-
-?>
+        if (isset($content) && $content) {
+          echo $content;
+        }
+    ?>
     <?php require_once("../includes/layouts/footer.php"); ?>
